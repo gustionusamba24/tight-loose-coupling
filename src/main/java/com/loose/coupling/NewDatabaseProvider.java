b134ba92c84f893c3da21a5a8776dcea287575ab
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-06-08 15:41:47 +0700

    update the changes

--- src/main/java/com/loose/coupling/NewDatabaseProvider.java
+++ src/main/java/com/loose/coupling/NewDatabaseProvider.java
@@ -1,6 +1,6 @@
 package com.loose.coupling;
 
-public class NewDatabaseProvider implements UserDataProvider{
+public class NewDatabaseProvider implements UserDataProvider {
 
     @Override
     public String getUserDetails() {
